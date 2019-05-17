6c4afa996a342a71050a169b70989fc2ad470276
Sosuke Suzuki <aosukeke@gmail.com>
2019-05-17 15:48:48 +0900

    スタイルをてきとうに適用

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React, { Suspense } from "react";
 import { mount, route } from "navi";
 import { Router, View, Link } from "react-navi";
+import styled, { createGlobalStyle } from "styled-components";
 import { createClient } from "contentful";
 import ContentfulContext from "../lib/ContentfulContext";
 import ArticleList from "./pages/ArticleList";
 import Article from "./pages/Article";
+import { black, blue, border } from "../lib/colors";
 
 const routes = mount({
   "/": route({
@@ -19,24 +21,79 @@ const routes = mount({
   })
 });
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: ${black};
+    font-family: 'M PLUS 1p', sans-serif;
+    a {
+      color: ${blue};
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+`;
+
+const Header = styled.header`
+  display: flex;
+  padding: 20px 0;
+  position: fixed;
+  height: 40px;
+  width: 100%;
+  top: 0;
+  background-color: white;
+  border-bottom: 1px solid ${border};
+  div {
+    width: 800px;
+    margin: 0 auto;
+    h1 {
+      margin: 0;
+      line-height: 40px;
+      font-size: 25px;
+    }
+    a {
+      color: ${black};
+      text-decoration: none;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+`;
+
+const Main = styled.main`
+  width: 800px;
+  margin: 0 auto;
+  margin-top: 80px;
+  padding: 30px 0;
+`;
+
 const App = () => {
   const client = createClient({
     space: process.env.CTF_SPACE_ID,
     accessToken: process.env.CTF_CDA_ACCESS_TOKEN
   });
   return (
-    <ContentfulContext.Provider value={{ client }}>
-      <Router routes={routes}>
-        <header>
-          <Link href="/">
-            <h1>鈴木颯介のブログ</h1>
-          </Link>
-        </header>
-        <Suspense fallback={null}>
-          <View />
-        </Suspense>
-      </Router>
-    </ContentfulContext.Provider>
+    <>
+      <GlobalStyle />
+      <ContentfulContext.Provider value={{ client }}>
+        <Router routes={routes}>
+          <Header>
+            <div>
+              <Link href="/">
+                <h1>鈴木颯介のブログ</h1>
+              </Link>
+            </div>
+          </Header>
+          <Main>
+            <Suspense fallback={null}>
+              <View />
+            </Suspense>
+          </Main>
+        </Router>
+      </ContentfulContext.Provider>
+    </>
   );
 };
 
